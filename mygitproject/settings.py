f11b3f1d8356c9b18aeab2c76db3a84d73ea5da5
rarillka <mariabragina08@gmail.com>
2025-05-15 21:28:20 +0300

    ?

--- mygitproject/settings.py
+++ mygitproject/settings.py
@@ -56,7 +56,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
-        
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
